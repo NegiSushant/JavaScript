43080d4ac5aec00c3beaabecd7c236a680ae5e28
NegiSushant <sushantsinghnegi1998@gmail.com>
2025-08-11 11:49:47 +0530

    functions completed

--- Functions in JS/Function.js
+++ Functions in JS/Function.js
@@ -154,3 +154,22 @@ bookEW(23, "Steven Williams");
 const bookEW23 = book.bind(eurowings, 23);
 bookEW23("Jonas Schmedtmann");
 bookEW23("Martha Cooper");
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value + value * 0.23;
+
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
